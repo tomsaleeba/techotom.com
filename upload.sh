89d18ee9c9563a1976fe6183254c4d26e213d66e
Tom Saleeba <tom.saleeba@gmail.com>
2020-03-17 16:04:45 +1100

    chore: add upload script

---
+++ upload.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -euxo pipefail
+
+awsProfile=${1:?first param must be AWS profile name}
+s3Bucket=${2:?second param must be S3 bucket name}
+
+for curr in index.html style.css; do
+  aws s3 cp $curr s3://$s3Bucket/ --acl=public-read --profile $awsProfile
+done
