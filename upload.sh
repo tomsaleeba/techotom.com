ecbeb547646dbe9b9f01d29eb7d3da0ec13e15aa
Tom Saleeba <tom.saleeba@gmail.com>
2020-04-05 22:17:11 +1000

    chore: add micropub details

--- upload.sh
+++ upload.sh
@@ -4,6 +4,6 @@ set -euxo pipefail
 awsProfile=${1:?first param must be AWS profile name}
 s3Bucket=${2:?second param must be S3 bucket name}
 
-for curr in index.html style.css; do
+for curr in index.html style.css public-pgp.key; do
   aws s3 cp $curr s3://$s3Bucket/ --acl=public-read --profile $awsProfile
 done
